b55e84cda1561a67c171cd4e188c60272d18199c
Wilgert Velinga <wilgert@wilgert.nl>
2020-04-12 16:31:08 +0200

    implement location lib

--- libs/location/src/lib/location.spec.ts
+++ libs/location/src/lib/location.spec.ts
@@ -1,7 +1,28 @@
 import { location } from './location';
+import MockedFunction = jest.MockedFunction;
+
+const mockGeolocation = {
+  getCurrentPosition: jest.fn((success): Position => success({position: {latitude:52.373062,longitude:4.892687}, timestamp:13371891 })),
+  watchPosition: jest.fn()
+};
+
+// @ts-ignore
+navigator.geolocation = mockGeolocation;
 
 describe('location', () => {
-  it('returns latitude and longtitude', () => {
-    expect(location()).toEqual('location');
+  it('calls the geolocation API with the right PositionOptions', async () => {
+    let positionOptions: PositionOptions = {
+      maximumAge: 50 * 1000,
+      timeout: 30 * 1000,
+      enableHighAccuracy: true
+    };
+
+    await location();
+    expect(navigator.geolocation.getCurrentPosition).toHaveBeenCalledWith(expect.any(Function), expect.any(Function), positionOptions)
+  });
+
+  it('returns a Position object', async () => {
+    const result = await location();
+    expect(result).toEqual({position: {latitude:52.373062,longitude:4.892687}, timestamp: 13371891});
   });
 });
--- libs/location/src/lib/location.ts
+++ libs/location/src/lib/location.ts
@@ -1,3 +1,9 @@
-export function location(): string {
-  return 'location';
+export function location(): Promise<Position> {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject, {
+      maximumAge: 50 * 1000,
+      timeout: 30 * 1000,
+      enableHighAccuracy: true
+    });
+  });
 }
