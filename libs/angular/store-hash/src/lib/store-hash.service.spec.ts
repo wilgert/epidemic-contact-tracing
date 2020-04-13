9e2f28717d71eb0c6ca71fb86b3e87e4d450ba16
Wilgert Velinga <wilgert@wilgert.nl>
2020-04-13 13:14:03 +0200

    provide mock NgxIndexedDBService in store-hash.service.spec.ts

--- libs/angular/store-hash/src/lib/store-hash.service.spec.ts
+++ libs/angular/store-hash/src/lib/store-hash.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoreHashService } from './store-hash.service';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
 
 describe('StoreHashService', () => {
   let service: StoreHashService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: NgxIndexedDBService, useValue: {} }]
+    });
     service = TestBed.inject(StoreHashService);
   });
 
