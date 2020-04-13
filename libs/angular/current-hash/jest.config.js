55f1194b6254f9c3e7a77f6ef390fd4d15f66232
Wilgert Velinga <wilgert@wilgert.nl>
2020-04-13 13:13:38 +0200

    fix paths in jest.config.js for current-hash and store-hash
    these were moved into a subfolder causing the `npm test` command to fail with a strange error:
    
      ```
      An unhandled exception occurred: ● Validation Error:
      Preset ../../jest.config.js is invalid:
      The "id" argument must be of type string. Received type object
      TypeError [ERR_INVALID_ARG_TYPE]: The "id" argument must be of type string. Received type object
        at validateString (internal/validators.js:125:11)
        at Module.require (internal/modules/cjs/loader.js:687:3)
        at require (internal/modules/cjs/helpers.js:25:18)
        at setupPreset (/opt/build/repo/node_modules/jest-config/build/normalize.js:348:14)
        at normalize (/opt/build/repo/node_modules/jest-config/build/normalize.js:703:15)
        at readConfig (/opt/build/repo/node_modules/jest-config/build/index.js:220:68)
      ```

--- libs/angular/current-hash/jest.config.js
+++ libs/angular/current-hash/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'current-hash',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/current-hash',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/libs/current-hash',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
--- libs/angular/store-hash/jest.config.js
+++ libs/angular/store-hash/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'store-hash',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/store-hash',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/libs/store-hash',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
